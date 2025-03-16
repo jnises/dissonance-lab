f8785f6cccd43c6b5ebeef3efaa3200bac7f4fcc
Joel Nises <joel.nises@gmail.com>
2025-03-16 20:35:42 +0100

    show notes

--- src/interval_display.rs
+++ src/interval_display.rs
@@ -1,9 +1,10 @@
 use crate::{
-    piano_gui, theory::{self, Interval},
+    piano_gui,
+    theory::{self, Interval},
     utils::{colorgrad_to_egui, colorous_to_egui},
 };
 use colorgrad::Gradient;
-use egui::{Align2, Color32, FontId, Rect, Sense, Stroke, Ui, Vec2, pos2, vec2};
+use egui::{Align2, Color32, FontId, Rect, Sense, Stroke, StrokeKind, Ui, Vec2, pos2, vec2};
 
 pub fn show(piano: &mut piano_gui::PianoGui, ui: &mut Ui) -> Option<piano_gui::Action> {
     let (action, piano_rect) = piano.show(ui);
@@ -29,15 +30,27 @@ pub fn show(piano: &mut piano_gui::PianoGui, ui: &mut Ui) -> Option<piano_gui::A
             let this_selected = semi == selected_semi;
             let score_center_pos = pos - Vec2::Y * ((row as f32 + 0.5) * key_width + 10.0);
             if this_selected {
-                painter.circle_filled(score_center_pos, key_width / 2.0, Color32::BLUE);
+                painter.rect_stroke(
+                    Rect::from_center_size(score_center_pos, Vec2::splat(key_width)),
+                    0.0,
+                    Stroke::new(2.0, Color32::WHITE),
+                    StrokeKind::Inside,
+                );
+                painter.text(
+                    score_center_pos,
+                    Align2::CENTER_CENTER,
+                    "♪",
+                    FontId::monospace(20.0),
+                    Color32::WHITE,
+                );
             } else {
-                let normalized_dissonance = interval.compound_dissonance() / Interval::Tritone.compound_dissonance();
+                let normalized_dissonance =
+                    interval.compound_dissonance() / Interval::Tritone.compound_dissonance();
                 painter.rect_filled(
                     Rect::from_center_size(score_center_pos, Vec2::splat(key_width)),
                     0.0,
                     colorous_to_egui(
-                        colorous::YELLOW_ORANGE_RED
-                            .eval_continuous(normalized_dissonance as f64),
+                        colorous::YELLOW_ORANGE_RED.eval_continuous(normalized_dissonance as f64),
                     ),
                 );
                 painter.text(
@@ -50,10 +63,7 @@ pub fn show(piano: &mut piano_gui::PianoGui, ui: &mut Ui) -> Option<piano_gui::A
                 painter.text(
                     score_center_pos - vec2(0.0, key_width / 2.0 - 20.0),
                     Align2::CENTER_TOP,
-                    format!(
-                        "{:+}¢",
-                        interval.tempered_just_error_cents() as i32
-                    ),
+                    format!("{:+}¢", interval.tempered_just_error_cents() as i32),
                     FontId::monospace(12.0),
                     Color32::from_black_alpha(180),
                 );
