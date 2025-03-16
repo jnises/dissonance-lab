187d9d08bcac934043183f2eb1f32ad1b5ae909e
Joel Nises <joel.nises@gmail.com>
2025-03-16 19:59:28 +0100

    chords

--- src/interval_display.rs
+++ src/interval_display.rs
@@ -14,52 +14,57 @@ pub fn show(piano: &mut piano_gui::PianoGui, ui: &mut Ui) -> Option<piano_gui::A
     ui.allocate_rect(interval_rect, Sense::empty());
     let painter = ui.painter();
     let key_width = interval_rect.width() / 12.0;
-    let selected_semi = piano
+    for (row, selected_semi) in piano
         .selected_keys()
-        .first_one()
-        .map(|i| i8::try_from(i).unwrap());
-    for semi in 0..12i8 {
-        let interval =
-            selected_semi.map(|selected| theory::Interval::from_semitone_wrapping(semi - selected));
-        let pos = pos2(
-            interval_rect.left() + key_width * (semi as f32 + 0.5),
-            interval_rect.bottom(),
-        );
-        let this_selected = Some(semi) == selected_semi;
-        painter.line_segment(
-            [pos, pos - vec2(0.0, 10.0)],
-            Stroke::new(
-                1.0,
-                if this_selected {
-                    Color32::BLUE
-                } else {
-                    Color32::GRAY
-                },
-            ),
-        );
-        let score_center_pos = pos - Vec2::Y * (key_width / 2.0 + 10.0);
-        if this_selected {
-            painter.circle_filled(score_center_pos, key_width / 2.0, Color32::BLUE);
-        } else if let Some(interval) = interval {
-            painter.rect_filled(
-                Rect::from_center_size(score_center_pos, Vec2::splat(key_width)),
-                0.0,
-                colorous_to_egui(
-                    colorous::YELLOW_ORANGE_RED
-                        .eval_continuous(interval.compound_dissonance() as f64),
+        .iter_ones()
+        .map(|i| i8::try_from(i).unwrap()).enumerate()
+    {
+        // let selected_semi = piano
+        //     .selected_keys()
+        //     .first_one()
+        //     .map(|i| i8::try_from(i).unwrap());
+        for semi in 0..12i8 {
+            let interval = theory::Interval::from_semitone_wrapping(semi - selected_semi);
+            let pos = pos2(
+                interval_rect.left() + key_width * (semi as f32 + 0.5),
+                interval_rect.bottom(),
+            );
+            let this_selected = semi == selected_semi;
+            painter.line_segment(
+                [pos, pos - vec2(0.0, 10.0)],
+                Stroke::new(
+                    1.0,
+                    if this_selected {
+                        Color32::BLUE
+                    } else {
+                        Color32::GRAY
+                    },
                 ),
             );
-            painter.text(
-                score_center_pos,
-                Align2::CENTER_CENTER,
-                format!(
-                    "{}\n{:+}¢",
-                    interval.just_ratio(),
-                    interval.tempered_just_error_cents() as i32
-                ),
-                egui::FontId::default(),
-                Color32::BLACK,
-            );
+            let score_center_pos = pos - Vec2::Y * ((row as f32 + 0.5) * key_width + 10.0);
+            if this_selected {
+                painter.circle_filled(score_center_pos, key_width / 2.0, Color32::BLUE);
+            } else {
+                painter.rect_filled(
+                    Rect::from_center_size(score_center_pos, Vec2::splat(key_width)),
+                    0.0,
+                    colorous_to_egui(
+                        colorous::YELLOW_ORANGE_RED
+                            .eval_continuous(interval.compound_dissonance() as f64),
+                    ),
+                );
+                painter.text(
+                    score_center_pos,
+                    Align2::CENTER_CENTER,
+                    format!(
+                        "{}\n{:+}¢",
+                        interval.just_ratio(),
+                        interval.tempered_just_error_cents() as i32
+                    ),
+                    egui::FontId::default(),
+                    Color32::BLACK,
+                );
+            }
         }
     }
     action
