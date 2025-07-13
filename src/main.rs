b6a0d4818b9b2567961776a99ef946f2418305db
Joel Nises <joel.nises@gmail.com>
2025-07-13 21:09:48 +0200

    we don't support non-wasm targets anymore

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([650.0, 500.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-                    .expect("Failed to load icon"),
-            ),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Dissonance Lab",
-        native_options,
-        Box::new(|cc| Ok(Box::new(dissonance_lab::DissonanceLabApp::new(cc)))),
-    )
-}
-
 // When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
 fn main() {
