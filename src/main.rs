7905922f30779b6d51ec72526033160933298747
Joel Nises <joel.nises@gmail.com>
2025-07-13 21:10:23 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
